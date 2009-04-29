32468562f4f11eca37941736e43cff484219d8d0
Roel Verdult <roel@libnfc.org>
2009-04-29 12:58:29 +0000

    adding generic structure for loading devices

---
+++ devices.h
@@ -0,0 +1,36 @@
+/*
+ 
+Public platform independent Near Field Communication (NFC) library
+Copyright (C) 2009, Roel Verdult
+ 
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef _LIBNFC_DEVICES_H_
+#define _LIBNFC_DEVICES_H_
+
+#include "defines.h"
+#include "types.h"
+#include "dev_acr122.h"
+#include "dev_pn531.h"
+
+const static struct dev_callbacks dev_callbacks_list[] = {
+//  Driver Name        Connect                  Transceive                    Disconect
+  { "ACR122",          dev_acr122_connect,      dev_acr122_transceive,        dev_acr122_disconnect       },
+  { "PN531USB",        dev_pn531_connect,       dev_pn531_transceive,         dev_pn531_disconnect        }
+};
+
+#endif // _LIBNFC_DEVICES_H_
+
