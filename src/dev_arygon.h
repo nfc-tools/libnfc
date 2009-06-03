5631a560323044b832113ef8febba58edcca9435
Roel Verdult <roel@libnfc.org>
2009-06-03 08:11:28 +0000

    Adding ARYGON ADRA-USB/ADRB-USB reader support, part 2

---
+++ src/dev_arygon.h
@@ -0,0 +1,35 @@
+/*
+ 
+Public platform independent Near Field Communication (NFC) library
+Copyright (C) 2009, Roel Verdult
+ 
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef _LIBNFC_DEV_ARYGON_H_
+#define _LIBNFC_DEV_ARYGON_H_
+
+#include "defines.h"
+#include "types.h"
+
+// Functions used by developer to handle connection to this device
+dev_info* dev_arygon_connect(const ui32 uiIndex);
+void dev_arygon_disconnect(dev_info* pdi);
+
+// Callback function used by libnfc to transmit commands to the PN53X chip
+bool dev_arygon_transceive(const dev_spec ds, const byte* pbtTx, const ui32 uiTxLen, byte* pbtRx, ui32* puiRxLen);
+
+#endif // _LIBNFC_DEV_ARYGON_H_
+
