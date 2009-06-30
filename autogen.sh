51720401a1093996ce15b5de65a7741ea352895f
Roel Verdult <roel@libnfc.org>
2009-06-30 10:53:53 +0000

    FIX (under MacOSX) ./autogen.sh: line 11: libtoolize: command not found

--- autogen.sh
+++ autogen.sh
@@ -5,10 +5,22 @@ rm -f aclocal.m4 ltmain.sh
 
 touch README
 
+LIBTOOLIZE=libtoolize
+case `uname -s` in
+Darwin)
+  LIBTOOLIZE=glibtoolize
+  ACLOCAL_ARGS="-I /opt/local/share/aclocal/"
+  #  SKIP_PCSC="1"
+  ;;
+FreeBSD)
+  ACLOCAL_ARGS="-I /usr/local/share/aclocal/"
+  ;;
+esac
+
 echo "Running aclocal..." ; aclocal $ACLOCAL_FLAGS || exit 1
 echo "Running autoheader..." ; autoheader || exit 1
 echo "Running autoconf..." ; autoconf || exit 1
-echo "Running libtoolize..." ; (libtoolize --copy --automake || glibtoolize --automake) || exit 1
+echo "Running libtoolize..." ; $LIBTOOLIZE --copy --automake || exit 1
 echo "Running automake..." ; automake --add-missing --copy --gnu || exit 1
 
 if [ -z "$NOCONFIGURE" ]; then
