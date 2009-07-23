4b3291f6b19725a1edffd133dfc790a664170145
Roel Verdult <roel@libnfc.org>
2009-07-23 10:22:03 +0000

    VS stdint-files

---
+++ win32/stdint/stdbool.h
@@ -0,0 +1,14 @@
+#ifndef _STDBOOL_H_
+#define	_STDBOOL_H_	
+
+#define	__bool_true_false_are_defined	1
+
+#ifndef __cplusplus
+
+#define	false	0
+#define	true	1
+typedef	int	bool;
+
+#endif // __cplusplus
+
+#endif // _STDBOOL_H_
