0977dd1c0b7a55b9f3ef65afe4c0b3bbf9bbfd04
Romuald Conty <romuald@libnfc.org>
2009-09-14 08:47:31 +0000

    Fix CMake pkg-config generation: "Requires:" is now dynamic. Fix devices.h, #endif should not be followed by the name of the block. Fix CMake libnfc_usb compilation. Rename tools to examples in CMake file. (This patch comes from Fkooman, thanks !)

--- src/devices.h
+++ src/devices.h
@@ -25,22 +25,22 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>
 #include "types.h"
 #ifdef HAVE_PCSC_LITE
   #include "dev_acr122.h"
-#endif
+#endif /* HAVE_PCSC_LITE */
 #ifdef HAVE_LIBUSB
   #include "dev_pn531.h"
   #include "dev_pn533.h"
-#endif HAVE_LIBUSB
+#endif /* HAVE_LIBUSB */
 #include "dev_arygon.h"
 
 const static struct dev_callbacks dev_callbacks_list[] = {
 //  Driver Name        Connect                  Transceive                    Disconnect
 #ifdef HAVE_PCSC_LITE
   { "ACR122",          dev_acr122_connect,      dev_acr122_transceive,        dev_acr122_disconnect       },
-#endif
+#endif /* HAVE_PCSC_LITE */
 #ifdef HAVE_LIBUSB
   { "PN531USB",        dev_pn531_connect,       dev_pn531_transceive,         dev_pn531_disconnect        },
   { "PN533USB",        dev_pn533_connect,       dev_pn533_transceive,         dev_pn533_disconnect        },
-#endif HAVE_LIBUSB
+#endif /* HAVE_LIBUSB */
   { "ARYGON",          dev_arygon_connect,      dev_arygon_transceive,        dev_arygon_disconnect       }
 };
 
