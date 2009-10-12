30b0a20973f9dbe40579ad1ff6fb67b181f6cb52
Romuald Conty <romuald@libnfc.org>
2009-10-12 14:52:26 +0000

    Reformat copyright banner. Add @file doxygen comment to each file. Partially Issue 40.

--- src/rs232.c
+++ src/rs232.c
@@ -1,26 +1,29 @@
+/**
+ * Public platform independent Near Field Communication (NFC) library
+ * 
+ * Copyright (C) 2009, Roel Verdult
+ * 
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ * 
+ * 
+ * @file rs232.c
+ * @brief
+ */
+
 /*
- 
-Public platform independent Near Field Communication (NFC) library
-Copyright (C) 2009, Roel Verdult
- 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>
-
-*/
-
-/*
-Based on rs232-code written by Teunis van Beelen
-available: http://www.teuniz.net/RS-232/index.html
+Based on rs232-code written by Teunis van Beelen available:
+http://www.teuniz.net/RS-232/index.html
 */
 
 
