f09c9a37641b6d5f600a78c9fb005dc85ce578af
Romuald Conty <romuald@libnfc.org>
2010-01-29 15:04:32 +0000

    Fix compiler warnings, fix issue 52.

--- config.h.cmake
+++ config.h.cmake
@@ -1,3 +1,4 @@
 #cmakedefine PACKAGE_NAME "@PACKAGE_NAME@"
 #cmakedefine PACKAGE_VERSION "@PACKAGE_VERSION@"
 #cmakedefine PACKAGE_STRING "@PACKAGE_STRING@"
+#cmakedefine _XOPEN_SOURCE "@_XOPEN_SOURCE@"
