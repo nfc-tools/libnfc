3e16031877ec30a106e3cc6040af8003bea70ae6
Romuald Conty <romuald@libnfc.org>
2010-04-22 09:01:22 +0000

    Fix ARYGON driver build (missing comma).

--- libnfc/drivers.h
+++ libnfc/drivers.h
@@ -66,7 +66,7 @@ static const struct driver_callbacks drivers_callbacks_list[] = {
   { PN533_USB_DRIVER_NAME,  pn533_usb_pick_device, pn533_usb_list_devices,    pn533_usb_connect,   pn53x_usb_transceive,     pn53x_usb_disconnect    },
 #endif /* DRIVER_PN533_USB_ENABLED */
 #if defined (DRIVER_ARYGON_ENABLED)
-  { ARYGON_DRIVER_NAME,     arygon_pick_device,     arygon_list_devices,      arygon_connect,      arygon_transceive,        arygon_disconnect       }
+  { ARYGON_DRIVER_NAME,     arygon_pick_device,     arygon_list_devices,      arygon_connect,      arygon_transceive,        arygon_disconnect       },
 #endif /* DRIVER_ARYGON_ENABLED */
 #if defined (DRIVER_PN532_UART_ENABLED)
   { PN532_UART_DRIVER_NAME, pn532_uart_pick_device, pn532_uart_list_devices,  pn532_uart_connect,  pn532_uart_transceive,    pn532_uart_disconnect   },
