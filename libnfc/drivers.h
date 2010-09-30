6b679862e7f5e48082934c796abfe0c891009f54
Romuald Conty <romuald@libnfc.org>
2010-09-30 10:27:34 +0000

    Fix bad indent.
    Update issue 84
    Current indent command push these comments the next line.

--- libnfc/drivers.h
+++ libnfc/drivers.h
@@ -31,33 +31,27 @@
 
 #  if defined (DRIVER_ACR122_ENABLED)
 #    include "drivers/acr122.h"
-#  endif
-       /* DRIVER_ACR122_ENABLED */
+#  endif /* DRIVER_ACR122_ENABLED */
 
 #  if defined (DRIVER_PN531_USB_ENABLED) || defined (DRIVER_PN533_USB_ENABLED)
 #    include "drivers/pn53x_usb.h"
-#  endif
-       /* DRIVER_PN531_USB_ENABLED || DRIVER_PN533_USB_ENABLED */
+#  endif /* DRIVER_PN531_USB_ENABLED || DRIVER_PN533_USB_ENABLED */
 
 #  if defined (DRIVER_PN531_USB_ENABLED)
 #    include "drivers/pn531_usb.h"
-#  endif
-       /* DRIVER_PN531_USB_ENABLED */
+#  endif /* DRIVER_PN531_USB_ENABLED */
 
 #  if defined (DRIVER_PN533_USB_ENABLED)
 #    include "drivers/pn533_usb.h"
-#  endif
-       /* DRIVER_PN533_USB_ENABLED */
+#  endif /* DRIVER_PN533_USB_ENABLED */
 
 #  if defined (DRIVER_ARYGON_ENABLED)
 #    include "drivers/arygon.h"
-#  endif
-       /* DRIVER_ARYGON_ENABLED */
+#  endif /* DRIVER_ARYGON_ENABLED */
 
 #  if defined (DRIVER_PN532_UART_ENABLED)
 #    include "drivers/pn532_uart.h"
-#  endif
-       /* DRIVER_PN532_UART_ENABLED */
+#  endif /* DRIVER_PN532_UART_ENABLED */
 
 #  define DRIVERS_MAX_DEVICES         16
 #  define MAX_FRAME_LEN       264
@@ -67,28 +61,23 @@ static const struct driver_callbacks drivers_callbacks_list[] = {
 #  if defined (DRIVER_ACR122_ENABLED)
   {ACR122_DRIVER_NAME, &pn53x_callbacks_list, acr122_pick_device, acr122_list_devices, acr122_connect,
    acr122_transceive, acr122_disconnect},
-#  endif
-  /* DRIVER_ACR122_ENABLED */
+#  endif /* DRIVER_ACR122_ENABLED */
 #  if defined (DRIVER_PN531_USB_ENABLED)
   {PN531_USB_DRIVER_NAME, &pn53x_callbacks_list, pn531_usb_pick_device, pn531_usb_list_devices, pn531_usb_connect,
    pn53x_usb_transceive, pn53x_usb_disconnect},
-#  endif
-  /* DRIVER_PN531_USB_ENABLED */
+#  endif /* DRIVER_PN531_USB_ENABLED */
 #  if defined (DRIVER_PN533_USB_ENABLED)
   {PN533_USB_DRIVER_NAME, &pn53x_callbacks_list, pn533_usb_pick_device, pn533_usb_list_devices, pn533_usb_connect,
    pn53x_usb_transceive, pn53x_usb_disconnect},
-#  endif
-  /* DRIVER_PN533_USB_ENABLED */
+#  endif /* DRIVER_PN533_USB_ENABLED */
 #  if defined (DRIVER_ARYGON_ENABLED)
   {ARYGON_DRIVER_NAME, &pn53x_callbacks_list, arygon_pick_device, arygon_list_devices, arygon_connect,
    arygon_transceive, arygon_disconnect},
-#  endif
-  /* DRIVER_ARYGON_ENABLED */
+#  endif /* DRIVER_ARYGON_ENABLED */
 #  if defined (DRIVER_PN532_UART_ENABLED)
   {PN532_UART_DRIVER_NAME, &pn53x_callbacks_list, pn532_uart_pick_device, pn532_uart_list_devices, pn532_uart_connect,
    pn532_uart_transceive, pn532_uart_disconnect},
-#  endif
-  /* DRIVER_PN532_UART_ENABLED */
+#  endif /* DRIVER_PN532_UART_ENABLED */
 };
 
 #  ifdef DEBUG
