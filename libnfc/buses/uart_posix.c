0aa1ff309ea8f25927515422af34c1ab9c229b3d
Philippe Teuwen <yobibe@gmail.com>
2013-03-06 10:56:12 +0100

    Fix cppcheck warning "Variable is assigned a value that is never used"

--- libnfc/buses/uart_posix.c
+++ libnfc/buses/uart_posix.c
@@ -127,7 +127,7 @@ uart_flush_input(serial_port sp)
   }
   char *rx = malloc(available_bytes_count);
   // There is something available, read the data
-  res = read(UART_DATA(sp)->fd, rx, available_bytes_count);
+  read(UART_DATA(sp)->fd, rx, available_bytes_count);
   log_put(LOG_GROUP, LOG_CATEGORY, NFC_LOG_PRIORITY_DEBUG, "%d bytes have eatten.", available_bytes_count);
   free(rx);
 }
