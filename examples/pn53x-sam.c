181cd914d5d52ae11d6a8348af1ba57736ba0bd3
Philippe Teuwen <yobibe@gmail.com>
2013-03-07 08:51:35 +0100

    pn53x-sam: fix print_nfc_target

--- examples/pn53x-sam.c
+++ examples/pn53x-sam.c
@@ -175,7 +175,7 @@ main(int argc, const char *argv[])
         exit(EXIT_FAILURE);
       } else if (res == 1) {
         printf("The following ISO14443A tag (SAM) was found:\n");
-        print_nfc_target(nt, true);
+        print_nfc_target(&nt, true);
       } else {
         ERR("%s", "More than one ISO14442 tag found as SAM.");
         nfc_close(pnd);
