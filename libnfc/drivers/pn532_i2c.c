3cd26a18f1a36702a5f2840b5d9df55cbdb2b1ff
Philippe Teuwen <yobibe@gmail.com>
2013-06-15 00:04:06 +0200

    I2C: add Laurent Latil as author

--- libnfc/drivers/pn532_i2c.c
+++ libnfc/drivers/pn532_i2c.c
@@ -8,6 +8,7 @@
  * Copyright (C) 2010-2013 Philippe Teuwen
  * Copyright (C) 2012-2013 Ludovic Rousseau
  * Additional contributors of this file:
+ * Copyright (C) 2013      Laurent Latil
  *
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
