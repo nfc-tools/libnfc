f9417586c0c8e23058e463a1d30e47659dbe5519
Michal Suchanek <msuchanek@suse.de>
2017-07-14 16:19:26 +0200

    nfc-poll: print "Waiting for card removing..." before waiting

--- examples/nfc-poll.c
+++ examples/nfc-poll.c
@@ -146,6 +146,7 @@ main(int argc, const char *argv[])
   if (res > 0) {
     print_nfc_target(&nt, verbose);
     printf("Waiting for card removing...");
+    fflush(stdout);
     while (0 == nfc_initiator_target_is_present(pnd, NULL)) {}
     nfc_perror(pnd, "nfc_initiator_target_is_present");
     printf("done.\n");
